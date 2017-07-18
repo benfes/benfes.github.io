7e35afcc7ba94c063716384e2e8c7d9dbbd2f9ca
Ben <ben.fesili@gmail.com>
2017-07-18 22:42:26 +1200

    increased optimization with image minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var imageResize = require('gulp-image-resize');
 
 gulp.task('images', function(){
   return gulp.src('src/img/*.jpg')
-  .pipe(imagemin({progressive: true}))
+  .pipe(imageResize({
+    width: 1200,
+    height: 600,
+    crop: true,
+    upscale: false
+  }))
+  .pipe(imagemin({progressive: true, optimizationLevel: 5}))
   .pipe(gulp.dest('assets/img'));
 });
